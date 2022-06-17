b1faec1d76d0555100316f4ce795bb9a266323cd
yehuda <yehuda.ar.v@gmail.com>
2022-06-17 16:16:52 +0300

    m

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@
 const COPY_ALERT = "הטקסט הועתק בהצלחה";
 const NIKUD = ["ְ", "ִ", "ֵ", "ֶ", "ַ", "ָ", "", "ׁ", "", "ׂ", "ֹ", "ּ", "", "ֻ", "ֱ", "ֲ", "ֳ",];
 /*const TAAMIM =[              "",    ];*/
-const PUNCTUATINION = [".", ",", "?", "!", ";", ":", "'", "\"", "\\", "/", "`", "-",];
+const PUNCTUATION = [".", ",", "?", "!", ";", ":", "'", "\"", "\\", "/", "`", "-",];
 const PARENTHESES = ["(", ")", "{", "}", "[", "]", "<", ">"];
 
 var teTextArea = document.getElementById("text-area")
@@ -10,14 +10,33 @@ var btRemove = document.getElementById("remove");
 var btCopy = document.getElementById("copy-to-clipboard");
 var cbNikud = document.getElementById("nikud");
 var cbTaamim = document.getElementById("taamim");
-var cbPunctuatinion = document.getElementById("punctuation");
+var cbPunctuation = document.getElementById("punctuation");
 var cbParentheses = document.getElementById("parentheses");
 var cbExtraSpaces = document.getElementById("extra-spaces");
 var cbKtivMale = document.getElementById("ktiv-male");
 
 // todo 2 arrays prone to bug, use a dictionary
-var checkBoxes = [cbNikud, /*cbTaamim,*/ cbPunctuatinion, cbParentheses]
-var checkBoxesCharacters = [NIKUD, /*TAAMIM,*/ PUNCTUATINION, PARENTHESES]
+var checkBoxes = [cbNikud, /*cbTaamim,*/ cbPunctuation, cbParentheses]
+var checkBoxesCharacters = [NIKUD, /*TAAMIM,*/ PUNCTUATION, PARENTHESES]
+
+
+document.addEventListener('keydown', e => {
+    if (e.altKey && e.key.toLocaleLowerCase() == "n" ) {
+        cbNikud.checked = !cbNikud.checked;
+    }
+    if (e.altKey && e.key.toLocaleLowerCase() == "t" ) {
+        cbTaamim.checked = !cbTaamim.checked;
+    }
+    if (e.altKey && e.key.toLocaleLowerCase() == "p" ) {
+        cbPunctuation.checked = !cbPunctuation.checked;
+    }
+    if (e.altKey && e.key.toLocaleLowerCase() == "b" ) {
+        cbParentheses.checked = !cbParentheses.checked;
+    }
+    if (e.altKey && e.key.toLocaleLowerCase() == "s" ) {
+        cbExtraSpaces.checked = !cbExtraSpaces.checked;
+    }
+});
 
 
 
